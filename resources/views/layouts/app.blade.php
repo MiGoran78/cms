8b63584a3ebe1862320c43eab31fece7c118b7b1
migoran78 <mi.goran@hotmail.com>
2016-10-20 01:31:33 +0200

    up3

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+
+</div>
+
+@yield('footer')
+
+</body>
+</html>
