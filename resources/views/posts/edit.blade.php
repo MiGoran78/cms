635da22da4bc9b21a26af7ab78119c4089d74990
Goran Mijailovic <mi.goran@hotmail.com>
2016-11-10 10:18:10 +0100

    10/11-1

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,11 +2,25 @@
 
 @section('content')
 
+    <h1>Edit Post</h1>
 
-    <form method="post" action="/posts">
+    <form method="post" action="/posts/{{$post->id}}">
+
         {{ csrf_field() }}
-        <input type="text" name="title" placeholder="Enter title">
-        <input type="submit" name="submit">
+
+
+        <input type="hidden" name="_method" value="PUT">
+
+        <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
+        <input type="submit" value="UPDATE">
+    </form>
+
+
+    <form action="/post/{{$post->id}}">
+
+        <input type="hidden" name="_method" value="DELETE">
+
+        <input type="submit" value="DELETE">
     </form>
 
 
